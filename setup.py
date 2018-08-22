750535ed4cb8a6c4db458bbd24af155bf574f534
Chen Huang <chen_huang@apple.com>
2018-08-21 21:39:39 -0400

    Update readme

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-
 import os
 
 def package_files(directory):
@@ -11,37 +10,30 @@ def package_files(directory):
             paths.append(os.path.join('..', path, filename))
     return paths
 
-extra_files = package_files('maui/package_template')
+# extra_files = package_files('amipy/package_template')
 
 with open('README.md') as f:
     readme = f.read()
 
 setup(
-    name='apple-maui',
+    name='amipy',
     version='1.0.35',
-    description='Install MacOS programmatically', # short description
+    description='PythonAutomation for Amibroker', # short description
     long_description=readme, # long description from the readme file
-    license='Apple Internal', # for internal packages
+    license='MIT', # for internal packages
     author='Chen Huang',
-    author_email='chen_huang@apple.com',
-    url='https://stash.sd.apple.com/projects/BQA/repos/maui/', # wherever your code lives
-    packages=['maui', "maui.locallibs"], # or py_modules (see below for more details)
+    author_email='chinux@gmail.com',
+    url='https://github.com/chinux23/amipy', # wherever your code lives
+    packages=['amipy'], # or py_modules (see below for more details)
     package_data={'maui': extra_files},
     # data_files=[('package_template', ['*.jpeg'])],
     # include_package_data=True,
     install_requires = [
-        "tqdm",                     # Used for local restore - progress bar.
-        "requests",                 # Used for heartbeat
-        "paramiko>=2.4.1",          # Used for remote SSH
-        "scp",                      # Used for remote restore
-        "AppleConnectUtils==0.1.0", # Used for appleconnect
-        "netifaces",                # Used for heartbeat,
-        "setuptools>=40.0.0",       # Used by osascript
+
     ], # any dependencies internal or world (OPTIONAL)
     entry_points={
         'console_scripts': [
-            'maui = maui.maui:main',
-            'createuserpkg = maui.createuserpkg:main'
+            'amipy = amipy.amipy:main',
         ]
     }
 )
